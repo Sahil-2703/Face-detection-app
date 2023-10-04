4185aba2260c5a8055ada70dcbb9f1e9c3f3e216
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-10-04 20:55:29 +0530

    Face-detection-app

---
+++ script.js
@@ -0,0 +1,22 @@
+const video = document.getElementById("video")
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo)
+
+
+async function startVideo() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+        video.srcObject = stream;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+video.addEventListener('play', ()=> {
+    console.log('started')
+})
